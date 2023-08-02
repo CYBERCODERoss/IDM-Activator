1d44effeb7f50d31ae5d02d64bf33687cb8eb92a
nautilus._.shell <110402662+CYBERCODERoss@users.noreply.github.com>
2023-08-02 18:49:07 +0500

    IAS.ps1

--- IAS.ps1
+++ IAS.ps1
@@ -1,7 +1,7 @@
 # Enable TLSv1.2 for compatibility with older clients
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
 
-$DownloadURL = 'https://raw.githubusercontent.com/CYBERCODERoss/IDM-Activator/main/IDM_2.0.cmd'
+$DownloadURL = 'https://raw.githubusercontent.com/CYBERCODERoss/IDM-Activator/main/IAS_2.0.cmd'
 
 $FilePath = "$env:TEMP\IAS.cmd"
 
